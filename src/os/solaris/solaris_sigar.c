083b64e164448aaf0bac12d5ce715f805bb33684
Brendan Gregg <brendan.gregg@joyent.com>
2012-08-27 10:28:12 -0700

    Joyent patch for various quirks in os/solaris.
    SIGAR should now do a better job of reporting memory & disk stats when running
    in a multi-tenant virtualized environment.

--- src/os/solaris/solaris_sigar.c
+++ src/os/solaris/solaris_sigar.c
@@ -34,6 +34,8 @@
 #include <sys/utsname.h>
 #include <dlfcn.h>
 #include <dirent.h>
+#include <sys/vm_usage.h>
+#include <zone.h>
 
 #define PROC_ERRNO ((errno == ENOENT) ? ESRCH : errno)
 #define SIGAR_USR_UCB_PS "/usr/ucb/ps"
@@ -171,6 +173,73 @@ char *sigar_os_error_string(sigar_t *sigar, int err)
     }
 }
 
+/* see usr/src/cmd/zonestat/zonestatd/zonestatd.c */
+typedef struct sigar_vmusage64 {
+        id_t vmu_zoneid;
+        uint_t vmu_type;
+        id_t vmu_id;
+        int vmu_align_next_members_on_8_bytes;
+        uint64_t vmu_rss_all;
+        uint64_t vmu_rss_private;
+        uint64_t vmu_rss_shared;
+        uint64_t vmu_swap_all;
+        uint64_t vmu_swap_private;
+        uint64_t vmu_swap_shared;
+} sigar_vmusage64_t;
+
+/* getvmusage() can be CPU expensive; throttle calls to secs: */
+#define VMUSAGE_INTERVAL       60
+
+static int zone_mem_get(sigar_t *sigar, sigar_mem_t *mem)
+{
+    kstat_ctl_t *kc = sigar->kc; 
+    kstat_t *ksp;
+    char path[MAXPATHLEN];
+    sigar_vmusage64_t result;
+    size_t nres = 1;
+    sigar_uint64_t used = 0, cap = 0;
+    int ret;
+
+    /*
+     * The memory_cap kstat is ideal (originally added to SmartOS), but
+     * if that doesn't exist, switch to getvmusage().
+     */
+    if ((ksp = kstat_lookup(sigar->kc, "memory_cap", -1, NULL)) &&
+        (kstat_read(sigar->kc, ksp, NULL) != -1))
+    {
+        kstat_named_t *kn;
+
+        if ((kn = (kstat_named_t *)kstat_data_lookup(ksp, "rss"))) {
+            used = kn->value.i64;
+        }
+        if ((kn = (kstat_named_t *)kstat_data_lookup(ksp, "physcap"))) {
+            cap = kn->value.i64;
+        }
+    }
+    else {
+        ret = sysinfo(SI_ARCHITECTURE_64, path, sizeof (path));
+        if (ret < 0) {
+                /* 32-bit kernels not supported via getvmusage() */
+                return -1;
+        }
+
+        /* since we are not GZ, should only get one result */
+        if (getvmusage(VMUSAGE_ZONE, VMUSAGE_INTERVAL, (vmusage_t *)&result,
+            &nres) != 0 || nres != 1) {
+            return -1;
+        }
+        used = result.vmu_rss_all;
+        cap = mem->total;
+    }
+
+    mem->actual_free = mem->free = cap - used;
+    mem->actual_used = mem->used = used;
+
+    sigar_mem_calc_ram(sigar, mem);
+
+    return SIGAR_OK;
+}
+
 int sigar_mem_get(sigar_t *sigar, sigar_mem_t *mem)
 {
     kstat_ctl_t *kc = sigar->kc;
@@ -185,6 +254,11 @@ int sigar_mem_get(sigar_t *sigar, sigar_mem_t *mem)
 
     if (sigar_kstat_update(sigar) == -1) {
         return errno;
+    }
+
+    if (getzoneid() != GLOBAL_ZONEID) {
+        /* zone-aware */
+       return (zone_mem_get(sigar, mem));
     }
 
     if ((ksp = sigar->ks.syspages) && kstat_read(kc, ksp, NULL) >= 0) {
@@ -1716,26 +1790,27 @@ int sigar_disk_usage_get(sigar_t *sigar, const char *name,
         id = SIGAR_FSDEV_ID(sb);
         ent = sigar_cache_get(sigar->fsdev, id);
         if (ent->value == NULL) {
-            return ENXIO;
+            status = ENXIO;
         }
-        iodev = (iodev_t *)ent->value;
+        else {
+            iodev = (iodev_t *)ent->value;
 
-        status = sigar_kstat_disk_usage_get(sigar, iodev->name, disk, &ksp);
+            status = sigar_kstat_disk_usage_get(sigar, iodev->name, disk, &ksp);
+        }
     }
     else {
         status = sigar_kstat_disk_usage_get(sigar, name, disk, &ksp);
-        if (status != SIGAR_OK) {
-            return status;
-        }
-        id = simple_hash(name); /*XXX*/
-        ent = sigar_cache_get(sigar->fsdev, id);
-        if (ent->value) {
-            iodev = (iodev_t *)ent->value;
-        }
-        else {
-            ent->value = iodev = malloc(sizeof(*iodev));
-            SIGAR_SSTRCPY(iodev->name, name);
-            SIGAR_DISK_STATS_INIT(&iodev->disk);
+        if (status == SIGAR_OK) {
+            id = simple_hash(name); /*XXX*/
+            ent = sigar_cache_get(sigar->fsdev, id);
+            if (ent->value) {
+                iodev = (iodev_t *)ent->value;
+            }
+            else {
+                ent->value = iodev = malloc(sizeof(*iodev));
+                SIGAR_SSTRCPY(iodev->name, name);
+                SIGAR_DISK_STATS_INIT(&iodev->disk);
+            }
         }
     }
 
@@ -1794,6 +1869,11 @@ int sigar_disk_usage_get(sigar_t *sigar, const char *name,
         }
 
         memcpy(&iodev->disk, disk, sizeof(iodev->disk));
+    }
+
+    if (status == ENXIO) {
+        /* Virtual device. This has no physical device mapping. */
+        return SIGAR_OK;
     }
 
     return status;
