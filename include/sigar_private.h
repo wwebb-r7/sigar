1bfb388f7e4165c0b3b821fdace75d074e57f504
David Cox <dave@boundary.com>
2016-04-22 09:44:02 -0500

    Fix compile issue with gcc-5 and inline declarations

--- include/sigar_private.h
+++ include/sigar_private.h
@@ -75,7 +75,7 @@
 #if defined(WIN32)
 #   define SIGAR_INLINE __inline
 #elif defined(__GNUC__)
-#   define SIGAR_INLINE inline
+#   define SIGAR_INLINE __attribute__ ((gnu_inline)) inline
 #else
 #   define SIGAR_INLINE
 #endif
